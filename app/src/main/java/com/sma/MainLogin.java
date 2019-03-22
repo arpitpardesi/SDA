e7eab67b4771ace7f3cfb23c6dbf9b9bd4521e00
Arpit Pardesi <arpit.pardesi6@gmail.com>
2019-03-22 17:23:05 +0530

    User signup

--- app/src/main/java/com/sma/MainLogin.java
+++ app/src/main/java/com/sma/MainLogin.java
@@ -1,5 +1,6 @@
 package com.sma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainLogin extends AppCompatActivity {
+public class MainLogin extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +17,18 @@ public class MainLogin extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        findViewById(R.id.textviewLI).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.textviewLI:
+                startActivity(new Intent(this, MainSignUp.class));
+                break;
+        }
     }
 
 }
--- app/src/main/java/com/sma/MainSignUp.java
+++ app/src/main/java/com/sma/MainSignUp.java
@@ -2,13 +2,27 @@ package com.sma;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainSignUp extends AppCompatActivity implements View.OnClickListener {
+    EditText editTextEmail, editTextPassword;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,18 +32,65 @@ public class MainSignUp extends AppCompatActivity implements View.OnClickListene
 
         findViewById(R.id.textviewSU).setOnClickListener(this);
 
+        editTextEmail =(EditText) findViewById(R.id.editTextEmail);
+        editTextPassword =(EditText) findViewById(R.id.editTextPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.buttonsignup:
+                registerUser();
+                break;
+
             case R.id.textviewSU:
-
                 startActivity(new Intent(this, MainLogin.class));
-
-
                 break;
         }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password= editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            editTextEmail.setError("Email required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            editTextPassword.setError("Password required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length()<6){
+            editTextPassword.setError("Minimum length of password is 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"User Resitered Successful", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 }
