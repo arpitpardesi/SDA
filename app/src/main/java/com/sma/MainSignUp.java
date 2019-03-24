be63f867020f3d1533b262982f9fd6bd43a12a5a
arpitpardesi <arpit.pardesi6@gmail.com>
2019-03-25 01:53:39 +0530

    Google Signup

--- app/src/main/java/com/sma/MainSignUp.java
+++ app/src/main/java/com/sma/MainSignUp.java
@@ -13,17 +13,27 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class MainSignUp extends AppCompatActivity implements View.OnClickListener {
-    private EditText editTextE;
-    private EditText editTextP;
+    EditText editTextE;
+    EditText editTextP;
     private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton buttonsi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +41,121 @@ public class MainSignUp extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_main_sign_up);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         findViewById(R.id.textviewSU).setOnClickListener(this);
 
+        editTextE = (EditText) findViewById(R.id.editTextEmail);
+        editTextP = (EditText) findViewById(R.id.editTextPassword);
 
-/*
-        editTextE =(EditText) findViewById(R.id.editTextEmail);
-        editTextP =(EditText) findViewById(R.id.editTextPassword);
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         mAuth = FirebaseAuth.getInstance();
-*/      //findViewById(R.id.buttonSU).setOnClickListener(this);
+/*
+        buttonsi = findViewById(R.id.googlesignup);
+        buttonsi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 101);
+            }
+        });
+*/
+
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+/*
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 101);
+    }
+*/
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 101) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                //assert account != null;
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(getApplicationContext(), MainState.class);
+                            startActivity(i);
+                            finish();
+                            Toast.makeText(getApplicationContext(), "User signup successfully", Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), "User SignUp Failed", Toast.LENGTH_SHORT).show();
+                        }
+                        // If sign in fails, display a message to the user.
+
+
+                    }
+
+                    // ...
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textviewSU:
+                startActivity(new Intent(this, MainLogin.class));
+                break;
+        }
+    }
+
+    public void GoogleSU(View view) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 101);
+    }
+}
+
+
+
+
+//Used below code earlier
+
+/*
+ @Override
 
     public void onStart() {
         super.onStart();
@@ -85,15 +199,15 @@ public class MainSignUp extends AppCompatActivity implements View.OnClickListene
     public void onClick(View view) {
 
         switch (view.getId()){
-            /*case R.id.buttonsignup:
+              case R.id.buttonsignup:
                 registerUser();
                 break;
-*/
-            case R.id.textviewSU:
-                startActivity(new Intent(this, MainLogin.class));
-                break;
+
+        case R.id.textviewSU:
+        startActivity(new Intent(this, MainLogin.class));
+        break;
         }
-/*
+
         String email = editTextE.getText().toString().trim();
         String password= editTextP.getText().toString().trim();
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -112,7 +226,3 @@ public class MainSignUp extends AppCompatActivity implements View.OnClickListene
             }
         });
 */
-
-    }
-
-}
