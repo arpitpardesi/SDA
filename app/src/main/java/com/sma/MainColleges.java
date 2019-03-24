ed6031b3e44f06ce6bc8680cb6b60828b55cde07
arpitpardesi <arpit.pardesi6@gmail.com>
2019-03-24 19:44:01 +0530

    State Activity, university ...

--- app/src/main/java/com/sma/MainColleges.java
+++ app/src/main/java/com/sma/MainColleges.java
@@ -1,7 +1,11 @@
 package com.sma;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainColleges extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainColleges extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_colleges);
+        Configuration config = getResources().getConfiguration();
+
+        WebView webView = findViewById(R.id.webviewColl);
+        webView.loadUrl("www.google.com");
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
     }
 }
