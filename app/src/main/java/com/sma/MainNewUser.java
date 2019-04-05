042e62c2ac9bc5f0319ec246350889bc3fedb543
arpitpardesi <arpit.pardesi6@gmail.com>
2019-04-05 16:22:02 +0530

    RealTime Database connected successfully

--- app/src/main/java/com/sma/MainNewUser.java
+++ app/src/main/java/com/sma/MainNewUser.java
@@ -2,12 +2,65 @@ package com.sma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainNewUser extends AppCompatActivity {
+
+    EditText Name, Email, Roll;
+    Spinner Branch, Sect, Year;
+    Button Reg;
+    DatabaseReference databaseNSR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_new_user);
+
+        Name = findViewById(R.id.editTextName);
+        Email = findViewById(R.id.editTextEmail);
+        Roll = findViewById(R.id.editTextRoll);
+        Branch = findViewById(R.id.spinnerBranch);
+        Sect = findViewById(R.id.spinnerSect);
+        Year = findViewById(R.id.spinnerYear);
+
+        Reg = findViewById(R.id.buttonRegister);
+
+        databaseNSR = FirebaseDatabase.getInstance().getReference("Students");
+
+        Reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usertype();
+            }
+        });
+    }
+
+    private void usertype(){
+        String name = Name.getText().toString().trim();
+        String email = Email.getText().toString().trim();
+        String roll = Roll.getText().toString().trim();
+        String branch = Branch.getSelectedItem().toString().trim();
+        String sect = Sect.getSelectedItem().toString().trim();
+
+        String year = Year.getSelectedItem().toString().trim();
+
+
+        if(!TextUtils.isEmpty(name)){
+            NSR fData = new NSR(name, email, roll, branch, sect, year);
+            databaseNSR.child(roll).setValue(fData);
+            Toast.makeText(this, "Student added", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Enter name", Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/sma/NSR.java
@@ -0,0 +1,15 @@
+package com.sma;
+
+public class NSR {
+
+    String SName, SEmail, SRoll, SBranch, SSect, SYear;
+
+    public NSR(String SName, String SEmail, String SRoll, String SBranch, String SSect, String SYear) {
+        this.SName = SName;
+        this.SEmail = SEmail;
+        this.SRoll = SRoll;
+        this.SBranch = SBranch;
+        this.SSect = SSect;
+        this.SYear = SYear;
+    }
+}
