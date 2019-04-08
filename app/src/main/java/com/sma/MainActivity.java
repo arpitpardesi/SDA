6113f96094f3ed3a6542a1bcd0b65c8094f2b918
arpitpardesi <arpit.pardesi6@gmail.com>
2019-04-09 00:04:42 +0530

    UI for Main Activity and App color changed

--- app/src/main/java/com/sma/MainActivity.java
+++ app/src/main/java/com/sma/MainActivity.java
@@ -88,12 +88,12 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+/*
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+*/
         return super.onOptionsItemSelected(item);
     }
 
